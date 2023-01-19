74292b7240a9374e1532e38d5eff0b7abe7baa6b
Ajay-at-005173903299 <anurag.pandey17@gmail.com>
2023-01-19 16:23:24 +0530

    final

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
 import { Popover, Transition } from "@headlessui/react";
 import {
   ArrowPathIcon,
@@ -112,7 +112,8 @@ export default function Example() {
   };
   // /api/website-info
   console.log(serviceoffering);
-
+  const buttonRef = useRef();
+  const handleClickPanel = () => buttonRef.current?.click();
   return (
     <Popover className="relative">
       <div className="mx-auto max-w-7xl px-4 sm:px-6">
@@ -136,7 +137,7 @@ export default function Example() {
             </Popover.Button>
           </div>
           <Popover.Group as="nav" className="hidden space-x-10 md:flex">
-            <a
+            <a 
               href="#abts-us"
               className="text-base font-medium	 font-sm text-black-500 hover:text-gray-900"
             >
@@ -167,20 +168,13 @@ export default function Example() {
         </div>
       </div>
 
-      {/* <Transition
-        as={Fragment}
-        enter="duration-200 ease-out"
-        enterFrom="opacity-0 scale-95"
-        enterTo="opacity-100 scale-100"
-        leave="duration-100 ease-in"
-        leaveFrom="opacity-100 scale-100"
-        leaveTo="opacity-0 scale-95"
-      > */}
+    
         <Popover.Panel
           focus
           className="mbwrapper absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden"
         >
           <div className="z-50	 divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+           
             <div className="px-5 pt-5 pb-6">
               <div className="flex items-center justify-between">
                 <div>
@@ -193,16 +187,18 @@ export default function Example() {
                   />
                 </div>
                 <div className="-mr-2">
-                  <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <Popover.Button ref={buttonRef} className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                     <span className="sr-only">Close menu</span>
                     <XMarkIcon className="h-6 w-6" aria-hidden="true" />
                   </Popover.Button>
                 </div>
               </div>
+
               <div className="mt-6">
+            
                 <nav className="grid gap-y-8">
                   {solutions.map((item) => (
-                    <a
+                    <a  onClick={handleClickPanel} onKeyDown={handleClickPanel}
                       key={item.name}
                       href={item.href}
                       className="-m-3 flex items-center rounded-md p-3 hover:bg-gray-50"
@@ -235,13 +231,16 @@ export default function Example() {
                   Service Offerings
                 </a>
                 {resources.map((item) => (
-                  <a
+              <button onClick={()=>{console.log("hi")}}>
+                  <a 
                     key={item.name}
                     href={item.href}
                     className="text-base text-gray-900 hover:text-gray-700"
                   >
                     {item.name}
                   </a>
+                  
+              </button>
                 ))}
               </div>
               <div>
@@ -255,7 +254,6 @@ export default function Example() {
             </div>
           </div>
         </Popover.Panel>
-      {/* </Transition> */}
     </Popover>
   );
 }
